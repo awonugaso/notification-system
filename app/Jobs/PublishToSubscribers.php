954211e3abc8c847af2ebdc037b4a9ed16eafa3f
Sherif <awonugasherif@gmail.com>
2021-04-04 00:37:50 +0100

    Added Queue Job process

---
+++ app/Jobs/PublishToSubscribers.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Topic;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class PublishToSubscribers implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $topic;
+    protected $payLoad;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($topic, $data)
+    {
+        //
+        $this->topic = $topic;
+        $this->payLoad = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        
+        
+        $topic =  Topic::find($this->topic->id);
+        
+        foreach ($topic->subscribers as $subscriber) {
+                    $response = Http::post($subscriber->endpoint, ['data' => $this->payLoad]);
+                }
+    }
+}
